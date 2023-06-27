4f279b82b9f2d16c67743136b6f47b013bddf809
Robot <aomaymistov@gmail.com>
2023-06-27 18:28:17 +0300

    Изменил стили кнопки входа

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from "next/head";
 import Link from "next/link";
-import styles from '/styles/page.module.scss'
+import styles from '/styles/button.module.scss'
 
 const Home = () => {
     return (
